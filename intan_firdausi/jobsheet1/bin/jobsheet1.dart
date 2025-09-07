eb478b9f560174a0779bef7f33536a0d9e8caa64
Intan Firdausi <intanfir27@gmail.com>
2025-09-07 22:49:52 +0700

    Jobsheet 1 - Intan FIrdausi (Belajar Comment)

---
+++ intan_firdausi/jobsheet1/bin/jobsheet1.dart
@@ -0,0 +1,10 @@
+import 'package:jobsheet1/jobsheet1.dart' as jobsheet1;
+
+void main(List<String> arguments) {
+  //print('Hello world: ${jobsheet1.calculate()}!');
+  //satu line comment
+  /*
+  multi line comment
+  banyak line comment
+  */
+}
---
+++ intan_firdausi/jobsheet1/test/jobsheet1_test.dart
@@ -0,0 +1,8 @@
+import 'package:jobsheet1/jobsheet1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

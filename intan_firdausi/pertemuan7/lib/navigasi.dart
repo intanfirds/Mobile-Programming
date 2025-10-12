f29711fbd3f019eab1863888e34427e6a0ae0b13
Intan Firdausi <intanfir27@gmail.com>
2025-10-12 21:30:00 +0700

    Pertemuan 7 : Navigasi

---
+++ intan_firdausi/pertemuan7/lib/navigasi.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Halaman Utama')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Ke Halaman Kedua'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Halaman Kedua')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Kembali'),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+    );
+  }
+}
